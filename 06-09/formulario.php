45d3fbe8c5152c8c2a8b69ea769da1d0792ded24
SimonMayorga <31394981+SimonMayorga@users.noreply.github.com>
2017-09-06 17:06:18 -0300

    clase 06-09

---
+++ 06-09/formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form method="post" action="recibe.php">
+		nombre:
+		<input type="text" name="nombre">
+
+
+		<br>
+		Edad:
+		<input type="text" name="edad">
+		<br>
+		<input type="submit" value="enviar">
+		
+	</form>
+</body>
+</html>
